bcda5ca36b949b8d92d195dbb2977eee58b9d6d9
babisha04 <babishariya@gmail.com>
2026-07-06 10:58:24 +0530

    refactor : Verify Locking Between Two Synchronized Methods

--- MultiThreading/RaceConditions/PerformanceAnalysis.java
+++ MultiThreading/RaceConditions/PerformanceAnalysis.java
@@ -1,6 +1,4 @@
 package MultiThreading.RaceConditions;
-
-
 class Printer {
    synchronized  void print() {  //synchronized method
 
@@ -16,24 +14,56 @@ class Printer {
         }
                  
     }
+    synchronized  void colorPrint() {  //synchronized method
+
+        for(int i=0;i<5;i++){
+            System.out.println("starting...");
+          //  synchronized(this){ // synchronized block
+            System.out.println("color print "+i+" "+Thread.currentThread().getName());
+            try{
+            Thread.sleep(1000);
+            }
+            catch(Exception e){}
+       // }
+        }
+                 
+    }
    
 }
-public class PerformanceAnalysis implements Runnable {
+ class PrintTask implements Runnable {
     Printer printer;
-    public PerformanceAnalysis(Printer printer){
+    public PrintTask(Printer printer){
         this.printer=printer;
     }
     public void run(){
      printer.print();
-
     }
+}
+ class ColorTask implements Runnable {
+    Printer printer;
+    public ColorTask(Printer printer){
+        this.printer=printer;
+    }
+    public void run(){
+     printer.colorPrint();
+    }
+}
+public class PerformanceAnalysis  {
+   
     public static void main(String[] args) throws InterruptedException {
         Printer print=new Printer();
         Thread[] thread=new Thread[3];
         long start=System.currentTimeMillis();
 
         for(int i=0;i<3;i++){
-           thread[i]=new Thread(new PerformanceAnalysis(print),"Thread-"+i);
+           thread[i]=new Thread(new PrintTask(print),"Thread-"+i);
+            thread[i].start();
+        }
+         for(int i=0;i<3;i++){
+            thread[i].join();
+        }
+         for(int i=0;i<3;i++){
+           thread[i]=new Thread(new ColorTask(print),"Thread-"+i);
             thread[i].start();
         }
         for(int i=0;i<3;i++){
