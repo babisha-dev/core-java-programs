f468dbb659c3fbe713601837c9b8f70faca6d0b7
babisha04 <babishariya@gmail.com>
2026-07-12 23:03:42 +0530

    refactor : Implement Bounded Buffer (Capacity = 5) using notifyAll()

--- MultiThreading/ProducerConsumer/BusyWaiting.java
+++ MultiThreading/ProducerConsumer/BusyWaiting.java
@@ -1,29 +1,36 @@
 package MultiThreading.ProducerConsumer;
 
+import java.util.*;
+
 class Buffer{
-    int numbers;
-       boolean full;
+    Queue<Integer> buffer=new LinkedList<>();
+   // int numbers;
+      // boolean full;
    synchronized public void produce(int val){
-            while(full){
+        //while(full){
+            while(buffer.size()==5){
                 try{
               wait();
                 System.out.println("Out of wait");}
                 catch(Exception e){}
             }
-             numbers=val;
-             full=true;
+             buffer.add(val);
+             //numbers=val;
+             //full=true;
              notifyAll();
-          System.out.println("Produces:"+numbers+" by "+Thread.currentThread().getName());
+          System.out.println("Produces:"+val+" by "+Thread.currentThread().getName());
          
     }
    synchronized public void consumer(){
-    while(!full){
+    //while(!full){
+    while(buffer.isEmpty()){
         try{
         wait();
         System.out.println("Notified waiting thread");}
         catch(Exception e){}}
-    int val=numbers;
-    full=false;
+    int val=buffer.remove();
+   // int val=numbers;
+   // full=false;
     System.out.println("Consumes:"+val+" by "+Thread.currentThread().getName());
     notifyAll();
     }
@@ -59,20 +66,20 @@ public class BusyWaiting {
     public static void main(String[] args) throws InterruptedException{
         Buffer buffer=new Buffer();
         Thread t1=new Thread(new Producer(buffer));
-                Thread t2=new Thread(new Producer(buffer));
+               // Thread t2=new Thread(new Producer(buffer));
                         Thread t3=new Thread(new Consumer(buffer));
 
-                                Thread t4=new Thread(new Consumer(buffer));
+                               // Thread t4=new Thread(new Consumer(buffer));
         t1.start();
-        t2.start();
+       // t2.start();
         t3.start();
-        t4.start();
+       // t4.start();
 
 
         t3.join();
         t1.join();
-        t2.join();
-        t4.join();
+       // t2.join();
+       // t4.join();
     }
     
 }
