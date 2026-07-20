820011e98617fa36cab201dfc5b7d7a9e899c521
babisha04 <babishariya@gmail.com>
2026-07-20 09:48:20 +0530

    refactor : Replace with CopyOnWriteArrayList

--- MultiThreading/LockAPi/CopyonWriteArrayList.java
+++ MultiThreading/LockAPi/CopyonWriteArrayList.java
@@ -1,24 +1,24 @@
 package MultiThreading.LockAPi;
 
-import java.util.ArrayList;
-
+//import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
 class SharedArrayList {
-    ArrayList<Integer> segment;
+    CopyOnWriteArrayList<Integer> segment;
 }
 class Userk1 extends Thread{
    
-    ArrayList<Integer> segment;
-     public Userk1(ArrayList<Integer> segment){
+    CopyOnWriteArrayList<Integer> segment;
+     public Userk1(CopyOnWriteArrayList<Integer> segment){
         this.segment=segment;
      }
     public void run(){
          
          try{
        for(int i=1;i<=100;i++){
-        segment.add(i);
+        segment.add(i); // adding thread
          Thread.sleep(10); 
        }
-       System.out.println("Added"+segment.size()+" " + Thread.currentThread().getName());
+       System.out.println("Added "+segment.size()+" values to " + Thread.currentThread().getName());
     }
     catch(Exception e){
 
@@ -27,20 +27,33 @@ class Userk1 extends Thread{
 
 }
 class Userk2 extends Thread{
-   
-    ArrayList<Integer> segment;
-     public Userk2(ArrayList<Integer> segment){
+   private volatile boolean running=true;
+    CopyOnWriteArrayList<Integer> segment;
+     public Userk2(CopyOnWriteArrayList<Integer> segment){
         this.segment=segment;
+     }
+     public void shutdown(){
+        this.running=false;
      }
     public void run(){
         try{
-        while(true){
-            for(Integer val:segment){
+            while (running) {
+       // while(!Thread.currentThread().isInterrupted()){ // using interruption so the user2 never fall in infinite loop like using true.
+             //while(true){
+        
+            for(Integer val:segment){  // looping thread
                  int temp=val;
             }
             Thread.sleep(20);
-       System.out.println(segment.size()+" " + Thread.currentThread().getName());
-        }}
+        }
+                       System.out.println(segment.size()+" is the size from  " + Thread.currentThread().getName());
+}
+        catch (InterruptedException e) {
+            // Fix: Log a clean termination message instead of e.printStackTrace()
+                   System.out.println(segment.size()+" is the size from  " + Thread.currentThread().getName());
+
+            System.out.println(Thread.currentThread().getName() + " received shutdown signal while sleeping. Exiting gracefully.");
+        }
         catch(Exception e){
         e.printStackTrace();
     }}
@@ -48,14 +61,16 @@ class Userk2 extends Thread{
 }
 public class CopyonWriteArrayList {
     public static void main(String[] args) throws InterruptedException{
-        ArrayList<Integer> segment =new ArrayList<>();
+        CopyOnWriteArrayList<Integer> segment =new CopyOnWriteArrayList<>();
         Userk1 userk1=new Userk1(segment);
         Userk2 userk2=new Userk2(segment);
 
         userk1.start();
         userk2.start();
 
-        userk1.join();
+        userk1.join(); // waiting for user1 to finish
+       // userk2.interrupt(); // stop user 2 if user1 finished
+       userk2.shutdown();
         userk2.join();
 
         
